63cf59bf36083deb33ec016473f4aa19d1757172
admin <admin@Win7_1TB>
2017-05-17 11:21:51 +0300

    Creados,todos los metodos de Oferta, Empleado y Vehiculo

---
+++ Proyecto/src/BaseDatos/BD_Oferta.java
@@ -0,0 +1,93 @@
+package BaseDatos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
+import modelos.Oferta;
+public class BD_Oferta extends BD_Conecta{
+	private static Statement s;	
+	private static ResultSet reg;
+	
+	public BD_Oferta(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+	public Vector<Oferta> listarOfertas(){
+		String cadenaSQL="SELECT * from oferta";
+		Vector<Oferta> listaOferta=new Vector<Oferta>();
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			while ( reg.next()){
+				listaOferta.add(new Oferta(reg.getInt("cod_oferta"),reg.getInt("descuento"),reg.getString("descripcion")));
+			}
+			s.close();
+			this.cerrar();
+			return listaOferta;
+		}
+		catch ( SQLException e){	
+			this.cerrar();
+			return null;			
+		}
+	}
+	
+	public Vector<Oferta> listaOfertaXUsu(String email){
+		String cadenaSQL="SELECT * from menu WHERE cod_restaurante='"+email+"'";
+		Vector<Oferta> listaOferta=new Vector<Oferta>();
+		try{
+			this.abrir();
+			s=c.createStatement();
+			reg=s.executeQuery(cadenaSQL);
+			while ( reg.next()){
+				listaOferta.add(new Oferta(reg.getInt("cod_oferta"),reg.getInt("descuento"),reg.getString("descripcion")));
+			}
+			s.close();
+			this.cerrar();
+			return listaOferta;
+		}
+		catch ( SQLException e){
+			this.cerrar();
+			return null;			
+		}
+	}
+	
+	public  int añadir_oferta( Oferta of){	
+		String cadenaSQL="INSERT INTO oferta VALUES('" + of.getCod_oferta()+ "','" +
+		of.getDescuento()+"','"+of.getDescripcion() +"')"; 	
+		
+		try{
+		this.abrir();
+		s=c.createStatement();
+		int filas=s.executeUpdate(cadenaSQL);
+		s.close();
+		this.cerrar();
+		return filas;
+		}
+		catch ( SQLException e){
+			this.cerrar();
+			return -1;
+		}
+	}
+	
+	public int borrar_oferta(Oferta of){
+		String cadenaSQL="DELETE FROM oferta WHERE cod_oferta='"+of.getCod_oferta()+"' ";
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+		}
+		catch ( SQLException e){		
+			this.cerrar();
+			return -1;
+		}
+	}
+	
+	
+	
+}
---
+++ Proyecto/src/BaseDatos/BD_Vehiculo.java
@@ -0,0 +1,67 @@
+package BaseDatos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import modelos.Vehiculo;
+
+public class BD_Vehiculo extends BD_Conecta{
+	private static Statement s;	
+	private static ResultSet reg;
+	
+	public BD_Vehiculo(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int altaVehiculo(Vehiculo ve){
+		String cadenaSQL="INSERT INTO vehiculo VALUES('" + ve.getMatricula() +"','" +
+				ve.getCod_personal()+"')"; 	
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+			}
+			catch ( SQLException e){
+				this.cerrar();
+				return -1;
+			}
+	}
+	
+	public int bajaVehiculo(Vehiculo ve){
+		String cadenaSQL="DELETE FROM vehiculo WHERE cod_personal='"+ve.getMatricula()+"' ";
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+		}
+		catch ( SQLException e){		
+			this.cerrar();
+			return -1;
+		}
+	}
+		
+	public int modificarMatricula(String matricula,int codPer){
+		String cadenaSQL="UPDATE vehiculo SET `matriucla`= '" + matricula + "' WHERE `cod_personal`= '" + codPer+"' ";
+		try{
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+		}
+		catch ( SQLException e){	
+			this.cerrar();
+			return -1;
+		}
+	}
+	
+}
